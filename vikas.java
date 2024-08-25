4a94b2a6ca9edbe3d38c28d1043da92b2c27dcd4
pal-vikas <vikaspal964@gmail.com>
2024-08-25 19:40:40 +0530

    Added new 7 line

--- vikas.java
+++ vikas.java
@@ -2,6 +2,16 @@ class vikas{
 	public static void main(String[] args)
 	{
 		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+
+		
+		
 	}
 
 }
