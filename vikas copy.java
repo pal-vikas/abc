1fe1400efb72dfcd138e04f193ddc80385278230
pal-vikas <vikaspal964@gmail.com>
2024-08-27 10:12:11 +0530

    Added copy for vikas.java

---
+++ vikas copy.java
@@ -0,0 +1,17 @@
+class vikas{
+	public static void main(String[] args)
+	{
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+		System.out.println("My name is Vikas Pal");
+
+		
+		
+	}
+
+}
